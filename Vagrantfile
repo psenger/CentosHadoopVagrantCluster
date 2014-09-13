660f59345a8225f4fc8b55a8164584a30b44ae1f
psenger <philip.a.senger@cngrgroup.com>
2014-09-14 07:19:31 +1000

    Added private network, Hadoop user, host file, unpack hadoop to /usr/local/hadoop and build /etc/hadoop. Need to break down the puppet file and get the datanodes talking

--- Vagrantfile
+++ Vagrantfile
@@ -2,10 +2,11 @@
 # vi: set ft=ruby :
 
 boxes = [
-  { :name => :namenode,  :ip => '192.168.33.11', :ssh_port => 2201, :cpus => 1, :mem => 1024, :mac => "720002691342" },
-  { :name => :datanode1, :ip => '192.168.33.12', :ssh_port => 2202, :cpus => 1, :mem => 1024, :mac => "720002691353" },
-  { :name => :datanode2, :ip => '192.168.33.13', :ssh_port => 2202, :cpus => 1, :mem => 1024, :mac => "720002691364" },
-  { :name => :datanode3, :ip => '192.168.33.14', :ssh_port => 2202, :cpus => 1, :mem => 1024, :mac => "720002691375" },
+  { :name => :namenode,       :ip => '192.168.33.11', :ssh_port => 2201, :cpus => 1, :mem => 1024, :mac => "720002691332" },
+  { :name => :jobtrackernode, :ip => '192.168.33.12', :ssh_port => 2202, :cpus => 1, :mem => 1024, :mac => "720002691343" },
+  { :name => :datanode1,      :ip => '192.168.33.13', :ssh_port => 2203, :cpus => 1, :mem => 1024, :mac => "720002691354" }
+  #{ :name => :datanode2,  :ip => '192.168.33.14', :ssh_port => 2204, :cpus => 1, :mem => 1024, :mac => "720002691365" },
+  #{ :name => :datanode3,  :ip => '192.168.33.15', :ssh_port => 2205, :cpus => 1, :mem => 1024, :mac => "720002691376" }
 ]
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
@@ -16,7 +17,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
         config.vm.define opts[:name] do |config|
           config.vm.box = "CentOS-7.0-1406-x86_64"
           config.vm.box_url = "http://www.cngrgroup.com/VirtualBoxImage/CentOS-7.0-1406-x86_64.box"
-          config.vm.network :private_network, ip: opts[:ip], netmask: "255.255.255.0", auto_config: false
+          config.vm.network "private_network", ip: opts[:ip], netmask: "255.255.255.0" #, auto_config: false
           config.vm.hostname = "%s.vagrant" % opts[:name].to_s
           config.vm.provision "puppet" do |puppet|
                 # puppet.options = "--verbose --debug"
--- puppet/base­hadoop.pp
+++ puppet/base­hadoop.pp
@@ -9,6 +9,7 @@ if versioncmp($::puppetversion,'3.6.1') >= 0 {
 Exec {
 	path => ['/usr/local/bin','/usr/bin','/usr/local/sbin','/usr/sbin']
 }
+
 File {
 	owner => root,
 	group => root,
@@ -18,24 +19,28 @@ File {
 
 $hadoop_home = '/usr/local/hadoop'
 
-exec { 'install_java_8' :
-	command => 'rpm --install /vagrant/sync/jdk-8u20-linux-x64.rpm',
-	creates => '/usr/java/jdk1.8.0_20',
+package { ['java-1.7.0-openjdk','java-1.7.0-openjdk-devel']:
+	ensure => present,
 }
-  
-file { '/etc/profile.d/java-1.8.0_20.sh':
-	content => "export JAVA_HOME=/usr/java/latest
-export JRE_HOME=/usr/java/latest
-export PATH=$JAVA_HOME/bin:$PATH
-",
+file { '/etc/profile.d/java-1.7.sh':
+	content => 'export JAVA_HOME=/usr/lib/jvm/java\nexport JRE_HOME=/usr/lib/jvm/jre\nexport PATH=$PATH:$JAVA_HOME/bin\n',
 }
+# exec { 'install_java_8' :
+# 	command => 'rpm --install /vagrant/sync/jdk-8u20-linux-x64.rpm',
+# 	creates => '/usr/java/jdk1.8.0_20',
+# }  
+# file { '/etc/profile.d/java-1.8.0_20.sh':
+# 	content => "export JAVA_HOME=/usr/java/latest
+# export JRE_HOME=/usr/java/latest
+# export PATH=$JAVA_HOME/bin:$PATH
+# ",
+# }
 
 exec { 'unpack_hadoop' :
  	command => 'tar -xf /vagrant/sync/hadoop-2.5.0.tar.gz',
 	cwd     => '/usr/local/',
 	creates => '/usr/local/hadoop-2.5.0',
 	user    => root, 
-	require => File['/usr/local/hadoop'],
 }
 
 file { '/usr/local/hadoop':
@@ -43,3 +48,105 @@ file { '/usr/local/hadoop':
 	replace => true,
 	target  => '/usr/local/hadoop-2.5.0',
 }
+
+group { 'hadoop':
+  ensure => 'present',
+  gid    => '1001',
+}
+
+user { 'hadoop':
+  ensure           => 'present',
+  managehome 	   => true,
+  gid              => '1001',
+  home             => '/home/hadoop',
+  password         => '$6$nD4vMHXv$xlQYJuXUwZ.U0mD/6XmcuC6JmfQAj7hN759SuSMue2I6fePPXbzQChJDhRA3XAyO3YNS9SL50ul0ru/G.PWGE.',
+  password_max_age => '99999',
+  password_min_age => '0',
+  shell            => '/bin/bash',
+  uid              => '1001',
+  require 		   => [ Group['hadoop'] ],
+}
+
+file {'/home/hadoop':
+	ensure  => 'directory',
+	group   => 'hadoop',
+	owner   => 'hadoop',
+	mode    => '755',
+	require => [ Group['hadoop'], User['hadoop'] ],
+}
+
+file { '/etc/hadoop': 
+	group   => 'hadoop',
+	owner   => 'hadoop',
+	mode    => '755',
+	replace => true,
+	recurse => true,
+	source  => '/usr/local/hadoop-2.5.0/etc/hadoop',
+	require => [ Exec['unpack_hadoop'], User['hadoop'] ],
+}
+
+file { '/etc/profile.d/hadoop-env.sh': 
+	source  => '/etc/hadoop/hadoop-env.sh',
+	mode    => 755,
+	owner   => root,
+	group   => root,
+	require => [ File['/etc/hadoop'] ],
+}
+
+file { '/etc/profile.d/httpfs-env.sh': 
+	source  => '/etc/hadoop/httpfs-env.sh',
+	mode    => 755,
+	owner   => root,
+	group   => root,
+	require => [ File['/etc/hadoop'] ],
+}
+
+file { '/etc/profile.d/mapred-env.sh': 
+	source  => '/etc/hadoop/mapred-env.sh',
+	mode    => 755,
+	owner   => root,
+	group   => root,
+	require => [ File['/etc/hadoop'] ],
+}
+
+file { '/etc/profile.d/yarn-env.sh': 
+	source  => '/etc/hadoop/yarn-env.sh',
+	mode    => 755,
+	owner   => root,
+	group   => root,
+	require => [ File['/etc/hadoop'] ],
+}
+
+
+host { 'localhost':
+  ensure       => 'present',
+  host_aliases => ['localhost.localdomain', 'localhost' ],
+  ip           => '127.0.0.1',
+  target       => '/etc/hosts',
+}
+host { 'datanode1':
+  ensure => 'present',
+  ip     => '192.168.33.13',
+  target => '/etc/hosts',
+}
+host { 'datanode2':
+  ensure => 'present',
+  ip     => '192.168.33.14',
+  target => '/etc/hosts',
+}
+host { 'datanode3':
+  ensure => 'present',
+  ip     => '192.168.33.15',
+  target => '/etc/hosts',
+}
+host { 'jobtracker':
+  ensure => 'present',
+  ip     => '192.168.33.12',
+  target => '/etc/hosts',
+}
+host { 'namenode':
+  ensure => 'present',
+  ip     => '192.168.33.11',
+  target => '/etc/hosts',
+}
+
